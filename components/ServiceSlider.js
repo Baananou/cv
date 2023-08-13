a58fef4c8af16128a3c79b0aa710df9a93119033
baananou <bananoumohamed@live.fr>
2023-08-13 21:16:56 +0100

    fixed build error

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -1,34 +1,35 @@
+import { RxCrop, RxPencil2, RxDesktop, RxReader, RxRocket } from "react-icons";
 // data
 const serviceData = [
-  {
-    icon: <RxCrop />,
-    title: 'Branding',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
-  {
-    icon: <RxPencil2 />,
-    title: 'Design',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
-  {
-    icon: <RxDesktop />,
-    title: 'Development',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
-  {
-    icon: <RxReader />,
-    title: 'Copywriting',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
-  {
-    icon: <RxRocket />,
-    title: 'SEO',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
+	{
+		icon: <RxCrop />,
+		title: "Branding",
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	},
+	{
+		icon: <RxPencil2 />,
+		title: "Design",
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	},
+	{
+		icon: <RxDesktop />,
+		title: "Development",
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	},
+	{
+		icon: <RxReader />,
+		title: "Copywriting",
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	},
+	{
+		icon: <RxRocket />,
+		title: "SEO",
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	},
 ];
 
 const ServiceSlider = () => {
-  return <div>Service Slider</div>;
+	return <div>Service Slider</div>;
 };
 
 export default ServiceSlider;
