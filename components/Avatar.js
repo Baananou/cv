557e1c76e32dafb4c4eecb52035e1eca0f6b2fa3
baananou <bananoumohamed@live.fr>
2023-08-15 01:54:48 +0100

    fixed the avatar

--- components/Avatar.js
+++ components/Avatar.js
@@ -2,17 +2,23 @@
 import Image from "next/image";
 const Avatar = ({ width, height }) => {
 	return (
-		<div className="flex">
+		<div className="max-w-none">
+			{/* <Image
+				src={"/bgme.png"}
+				width={737}
+				height={678}
+				alt="Avatar"
+				className="translate-z-0 w-full h-full"
+			/> */}
 			<Image
 				src={"/me4.png"}
-				alt="ME"
+				alt=""
 				width={width}
 				height={height}
-				className={`border-2 rounded-full w-[${width}px] h-[${height}px] bg-black object-cover`}
+				className={`border-2 rounded-full w-[200px] h-[200px] xl:w-[350px] xl:h-[350px] bg-black object-cover`}
 			/>
 		</div>
 	);
 };
 
 export default Avatar;
-// 
