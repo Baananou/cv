fb1c66783e1c6bfbdc0a899777d3aa3a90385841
baananou <bananoumohamed@live.fr>
2023-08-15 01:37:58 +0100

    contact page ui done

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,7 +1,49 @@
+//components
+import Circles from "/components/Circles.js";
+
+// icons
+import { BsArrowRight } from "react-icons/bs";
+
+//framer
+import { motion } from "framer-motion";
+
+//variants
+import { fadeIn } from "../../variants.js";
+
 const Contact = () => {
-  return (
-		<div className="flex justify-center items-center h-full">
-			<h1 className="text-3xl text-accent">Coming Soon Contact Page...</h1>
+	return (
+		<div className="h-full bg-primary/30">
+			<div className="container mx-auto py-32 text-center xl:text-left flex items-center justify-center h-full">
+				<div className="flex flex-col w-full max-w-[700px] ">
+					<motion.h2
+						variants={fadeIn("up", 0.2)}
+						initial="hidden"
+						animate="show"
+						exit="hidden"
+						className="h2 text-center mb-12">
+						Let&apos;s <span className="text-accent">connect.</span>
+					</motion.h2>
+					<motion.form
+						variants={fadeIn("up", 0.2)}
+						initial="hidden"
+						animate="show"
+						exit="hidden"
+						className="flex-1 flex flex-col gap-6 w-full mx-auto">
+						<div className="flex gap-x-6 w-full">
+							<input type="text" placeholder="name" className="input" />
+							<input type="email" placeholder="email" className="input" />
+						</div>
+						<input type="text" placeholder="subject" className="input" />
+						<textarea className="textarea" placeholder="message"></textarea>
+						<button className="btn rounded-full border border-white/50 max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group">
+							<span className="group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500">
+								Let&apos; Talk
+							</span>
+							<BsArrowRight className="-translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px] group-hover:text-lightprime" />
+						</button>
+					</motion.form>
+				</div>
+			</div>
 		</div>
 	);
 };
