4b71af45a410698d7f3910f97efcc8947ff5992d
baananou <bananoumohamed@live.fr>
2023-08-18 00:13:43 +0100

    trying to improve seo

--- seo/next-seo.config.js
+++ seo/next-seo.config.js
@@ -2,20 +2,14 @@ const config = {
 	title: "Mohamed Yessine Baananou",
 	titleTemplate: "%s | Mohamed Yessine Baananou",
 	description: "",
-	// canonical: process.env.NEXT_PUBLIC_DOMAIN_NAME,
+	canonical: "https://baananou.netlify.app/",
 	openGraph: {
 		type: "website",
 		locale: "fr",
-		// url: process.env.NEXT_PUBLIC_DOMAIN_NAME,
+		url: "https://baananou.netlify.app/",
 		siteName: "Mohamed Yessine Baananou Personal Website",
 		title: "Mohamed Yessine Baananou Personal Website",
 	},
-
-	// twitter: {
-	// 	handle: "@afraahotel1",
-	// 	site: "@afraahotel1",
-	// 	cardType: "summary_large_image",
-	// },
 };
 
 export default config;
