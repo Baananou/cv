f4cdbd7e31cfdf46676734e0ee90f920be15b841
baananou <bananoumohamed@live.fr>
2023-08-17 20:57:00 +0100

    netlify

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-	output: "export",
 	reactStrictMode: true,
-	experimental: {
-		optimizeImages: true,
-	},
-	swcMinify: true,
 	images: {
 		domains: ["res.cloudinary.com"],
 	},
