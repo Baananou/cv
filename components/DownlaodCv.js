6bdeeb6d899f24e2c5d5d514f61b2eb559a976db
baananou <bananoumohamed@live.fr>
2023-08-21 21:40:53 +0100

    trying to fix download cv on mobile

--- components/DownlaodCv.js
+++ components/DownlaodCv.js
@@ -1,26 +1,43 @@
 import React from "react";
 import { MdSimCardDownload } from "react-icons/md";
 
-function DownlaodCv() {
+function DownloadCv() {
+	const handleDownload = () => {
+		// Construct the full URL to the PDF file
+		const pdfUrl = process.env.PUBLIC_URL + "/resume.pdf";
+
+		// Create an anchor element to trigger the download
+		const anchor = document.createElement("a");
+		anchor.href = pdfUrl;
+		anchor.target = "_blank"; // Open in a new tab
+		anchor.download = "resume.pdf"; // Specify the file name
+		anchor.rel = "noopener noreferrer";
+
+		// Append the anchor to the document and simulate a click
+		document.body.appendChild(anchor);
+		anchor.click();
+
+		// Clean up by removing the anchor element
+		document.body.removeChild(anchor);
+	};
+
 	return (
-		<a
+		<div
 			aria-label="CV"
-			className="-ml-4 xl:ml-0 xl:-mt-2 relative group"
-			href="/resume.pdf"
-			download="resume.pdf"
-			target="_blank"
-			rel="noopener noreferrer">
-			<MdSimCardDownload />
+			className="ml-[-0.75rem] xl:ml-0 xl:-mt-2 relative group"
+			onClick={handleDownload}
+			style={{ cursor: "pointer" }}>
+			<MdSimCardDownload className="text-primary" />
 			<div className="absolute pr-14 right-0 hidden xl:group-hover:flex -mt-6">
 				<div className="bg-white relative flex text-primary items-center p-[6px] rounded">
-					<div className="text-[12px] leading-none font-semibold capitalize text-center">
+					<div className="text-[12px] font-semibold capitalize text-center">
 						Télécharger CV
 					</div>
 					<div className="border-solid border-l-white border-l-8 border-y-transparent border-y-[6px] border-r-0 absolute -right-2"></div>
 				</div>
 			</div>
-		</a>
+		</div>
 	);
 }
 
-export default DownlaodCv;
+export default DownloadCv;
