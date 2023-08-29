4647b1a01482307ae0d885cc154772c63dcfa1aa
baananou <bananoumohamed@live.fr>
2023-08-29 16:39:05 +0100

    work page done

---
+++ components/Project.js
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import { motion, useScroll, useTransform } from "framer-motion";
+import Link from "next/link";
+
+export default function Project({ title, description, tags, imageUrl, repo }) {
+	return (
+		<motion.div className="group mb-3 sm:mb-8 last:mb-0">
+			<section className="bg-secondary/70 max-w-[50rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-secondary/95 transition sm:group-even:pl-8 ">
+				<div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[60%] flex flex-col h-full sm:group-even:ml-[18rem]">
+					<h3 className="text-2xl font-semibold">{title}</h3>
+					<p className="mt-2 leading-relaxed text-white/70">{description}</p>
+					<ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
+						{tags.map((tag, index) => (
+							<li
+								className="bg-primary px-3 py-2 text-[0.7rem] uppercase tracking-wider rounded-full text-white/70 flex justify-center items-center gap-2"
+								key={index}>
+								{tag.icon}
+								{tag.title}
+							</li>
+						))}
+					</ul>
+				</div>
+				<Image
+					src={imageUrl}
+					alt="Project I worked on"
+					quality={95}
+					width={1500}
+					height={1500}
+					className="absolute hidden sm:block top-8 -right-40 w-[25rem] rounded-lg shadow-2xl
+                                transition
+                                group-hover:scale-[1.04]
+                                group-hover:-translate-x-3
+                                group-hover:translate-y-3
+                                group-hover:-rotate-2
+                                group-even:group-hover:translate-x-3
+                                group-even:group-hover:translate-y-3
+                                group-even:group-hover:rotate-2
+                                group-even:right-[initial] group-even:-left-40"
+				/>
+				<div className="xl:absolute hidden  bottom-2 right-2 w-36 bg-black/[0.7] px-3 py-2 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70 xl:flex justify-center items-center gap-2">
+					<Link href={repo}>Consulter Repo</Link>
+				</div>
+			</section>
+		</motion.div>
+	);
+}
---
+++ components/Projects.js
@@ -0,0 +1,91 @@
+import React from "react";
+import Project from "./Project";
+import { TbBrandFirebase, TbBrandReactNative } from "react-icons/tb";
+import {
+	SiBootstrap,
+	SiDart,
+	SiDotnet,
+	SiExpo,
+	SiFirebase,
+	SiFlask,
+	SiFlutter,
+	SiGithub,
+	SiHtml5,
+	SiNetlify,
+	SiNextdotjs,
+	SiPython,
+	SiTailwindcss,
+} from "react-icons/si";
+
+export default function Projects() {
+	const projectsData = [
+		{
+			title: "React Native ToDo App",
+			description:
+				"Conception et développement d'une application mobile pour gérer les tâches.",
+			tags: [
+				{
+					icon: <TbBrandReactNative key={1} size={20} />,
+					title: "React Native",
+				},
+				{ icon: <TbBrandFirebase key={2} size={20} />, title: "Firebase" },
+				{ icon: <SiExpo key={3} size={20} />, title: "Expo" },
+			],
+			imageUrl:
+				"https://res.cloudinary.com/duaagiskm/image/upload/v1692305497/cv%20webp/thumb1_tjrbba_odwa6o.webp",
+			repo: "https://github.com/Baananou/React-Native-TODO-APP-EXPO-Firebase-",
+		},
+		{
+			title: "Weather App",
+			description:
+				"Développement d'une application mobile pour consulter la météo en utilisant open Weather Api.",
+			tags: [
+				{ icon: <SiFlutter key={1} size={20} />, title: "Flutter" },
+				{ icon: <SiDart key={2} size={20} />, title: "Dart" },
+			],
+			imageUrl:
+				"https://res.cloudinary.com/duaagiskm/image/upload/v1692305498/cv%20webp/thumb2_wsavkq_p5osrg.webp",
+			repo: "https://github.com/Baananou/flutter_weatherapp",
+		},
+		{
+			title: "Interface de contrôle",
+			description:
+				'Conception et développement d\'interface "Contrôle avancé de teinture du bois"',
+			tags: [
+				{ icon: <SiNextdotjs key={1} size={20} />, title: "Next.js" },
+				{ icon: <SiTailwindcss key={1} size={20} />, title: "Tailwind CSS" },
+				{ icon: <SiFirebase key={2} size={20} />, title: "Firebase" },
+				{ icon: <SiFlask key={3} size={20} />, title: "Flask" },
+				{ icon: <SiPython key={4} size={20} />, title: "Python" },
+			],
+			imageUrl:
+				"https://res.cloudinary.com/duaagiskm/image/upload/v1692305501/cv%20webp/thumb3_vuzu9x_iyu81y.webp",
+			repo: "https://github.com/Baananou/Processus-industriel-de-contr-le-et-de-commande-avanc-de-teinture-du-bois-FRONT",
+		},
+		{
+			title: "Personal website",
+			description:
+				"Conception et développement d'une application de Suivi et coordination optimisés des stages.",
+			tags: [
+				{ icon: <SiNextdotjs key={1} size={20} />, title: "Next.js" },
+				{ icon: <SiTailwindcss key={2} size={20} />, title: "Tailwind CSS" },
+				{ icon: <SiGithub key={3} size={20} />, title: "GitHub" },
+				{ icon: <SiNetlify key={4} size={20} />, title: "Netlify" },
+			],
+			imageUrl:
+				"https://res.cloudinary.com/duaagiskm/image/upload/v1692305502/cv%20webp/thumb4_zlbuym_lxjzcy.webp",
+			repo: "https://github.com/Baananou/cv",
+		},
+	];
+	return (
+		<section className="container mx-auto py-8 h-full scrollbar-track-current overflow-y-auto xl:scrollbar-none xl:overflow-clip">
+			<div className="grid xl:grid-cols-2  gap-6">
+				{projectsData.map((project, index) => (
+					<div key={index} className="flex justify-center">
+						<Project {...project} />
+					</div>
+				))}
+			</div>
+		</section>
+	);
+}
