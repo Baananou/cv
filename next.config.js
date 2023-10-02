7c5b65c204eb784fb294eaf21aa0e833cbaebc65
Baananou <bananoumohamed30@gmail.com>
2023-10-02 12:31:15 +0100

    updated next config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,20 @@
-/** @type {import('next').NextConfig} */
-const { i18n } = require("./i18n/next-i18next.config");
+module.exports = (phase, { defaultConfig }) => {
+	/**
+	 * @type {import('next').NextConfig}
+	 */
+	const { i18n } = require("./i18n/next-i18next.config");
 
-const nextConfig = {
-	i18n: {
-		...i18n,
-		localeDetection: false,
-	},
-	reactStrictMode: true,
-	trailingSlash: true,
-	output: "standalone",
-	images: {
-		domains: ["res.cloudinary.com"],
-	},
+	const nextConfig = {
+		i18n: {
+			...i18n,
+			localeDetection: false,
+		},
+		reactStrictMode: true,
+		trailingSlash: true,
+		output: "standalone",
+		images: {
+			domains: ["res.cloudinary.com"],
+		},
+	};
+	return nextConfig;
 };
-
-module.exports = nextConfig;
