bad122195c259ae101b769e7bd45dcb4674d1327
Baananou <bananoumohamed30@gmail.com>
2023-10-03 13:35:31 +0100

    hayyyya

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ module.exports = (phase, { defaultConfig }) => {
 		},
 		reactStrictMode: true,
 		trailingSlash: true,
-		output: "export",
+		// output: "export",
 		images: {
 			domains: ["res.cloudinary.com"],
 		},
